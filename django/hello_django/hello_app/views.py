622007954f833e8dede59ca07f815355c1ef2f51
Thomas Bolinger <bolitj01@pfw.edu>
2024-03-07 17:50:48 -0500

    Starter example completed

--- django/hello_django/hello_app/views.py
+++ django/hello_django/hello_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+# A basic view returing a string
+def home(request):
+    return HttpResponse("Hello, Django!")
