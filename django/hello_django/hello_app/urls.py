0d631b2cb051b5d16279986e278a8880cd9e9762
Thomas Bolinger <bolitj01@pfw.edu>
2024-03-07 18:09:17 -0500

    Documentation

--- django/hello_django/hello_app/urls.py
+++ django/hello_django/hello_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+# URL Configuration
 urlpatterns = [
+    # e.g. http://localhost:8000/
     path("", views.home, name="home"),
 ]
--- django/hello_django/hello_app/views.py
+++ django/hello_django/hello_app/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 
-# A basic view returing a string
+# A basic view returing a string with the current time
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return HttpResponse("Hello, Django! The time is: " + str(datetime.now().strftime("%H:%M:%S")))
