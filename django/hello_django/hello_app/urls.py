6446c4d4854f2984d32d524cf927f38a9f22e40a
Thomas Bolinger <bolitj01@pfw.edu>
2024-03-08 02:58:20 -0500

    Add HTTP GET and POST with tests

--- django/hello_django/hello_app/urls.py
+++ django/hello_django/hello_app/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     # e.g. http://localhost:8000/
     path("", views.home, name="home"),
+    # e.g. http://localhost:8000/headers
+    path("headers", views.headers, name="headers"),
+    # e.g. http://localhost:8000/data
+    path("data", views.data, name="data"),
 ]
